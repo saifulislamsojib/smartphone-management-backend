617d58eecaaadfbd7bd98a05624ac2171eabdc51
saifulislamsojib <saifulislam.sojib56@gmail.com>
2024-01-26 23:47:15 +0600

    [feat] password extra validation commented

--- src/modules/auth/auth.validation.ts
+++ src/modules/auth/auth.validation.ts
@@ -5,19 +5,19 @@ const passwordSchema = z
   .trim()
   .refine((password) => password.length >= 8, {
     message: 'Password must be at least 8 characters long',
-  })
-  .refine((password) => /[A-Z]/.test(password), {
-    message: 'Password must contain at least one uppercase letter',
-  })
-  .refine((password) => /[a-z]/.test(password), {
-    message: 'Password must contain at least one lowercase letter',
-  })
-  .refine((password) => /\d/.test(password), {
-    message: 'Password must contain at least one digit',
-  })
-  .refine((password) => /[!@#$%^&*(),.?":{}|<>]/.test(password), {
-    message: 'Password must contain at least one special character',
   });
+// .refine((password) => /[A-Z]/.test(password), {
+//   message: 'Password must contain at least one uppercase letter',
+// })
+// .refine((password) => /[a-z]/.test(password), {
+//   message: 'Password must contain at least one lowercase letter',
+// })
+// .refine((password) => /\d/.test(password), {
+//   message: 'Password must contain at least one digit',
+// })
+// .refine((password) => /[!@#$%^&*(),.?":{}|<>]/.test(password), {
+//   message: 'Password must contain at least one special character',
+// });
 
 export const registerUserSchema = z.object({
   name: z.string().trim().min(1).max(255),
